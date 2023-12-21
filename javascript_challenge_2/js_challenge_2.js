14842af3618417b763e904ddacc13a60e90a0e07
733325 <733325@sylvaniastudents.org>
2023-12-21 15:57:04 -0500

    done

---
+++ javascript_challenge_2/js_challenge_2.js
@@ -0,0 +1,13 @@
+
+document.getElementById("state").addEventListener("click", myFunction);
+
+function statebox() {
+let name=prompt("What state do you live in?!");
+if (name ="") {
+    name=prompt("What state do you live in?!");
+}
+}
+
+if (name ="") {
+    name=prompt("What state do you live in?!");
+}
